3d6c7d008fa74609e90d26e7cbff80cd0e46fb33
mvaqasuddin <mvaqasuddin@gmail.com>
2013-02-23 00:19:32 -0800

    Connection
    Connection

---
+++ application/config/database/database.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+'connections' => array(
+		'mysql' => array(
+			
+			'driver'   => 'mysql',
+			'host'     => 'tunnel.pagodabox.com:3306',
+			'database' => 'db1',
+			'username' => 'arminda',
+			'password' => 'K5kjsmRe',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+		)
+	)
+);
